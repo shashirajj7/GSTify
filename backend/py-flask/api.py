3b9b87a99fc8c84343b9bcb80d41063f94b7558e
shashirajj7 <shashiraj.792006@gmail.com>
2026-03-18 13:45:24 +0530

    added home route

--- backend/py-flask/api.py
+++ backend/py-flask/api.py
@@ -251,6 +251,10 @@ def generate_csv():
     
     return jsonify({"success": True, "message": "CSVs generated successfully"}), 200
 
+@app.route("/")
+def home():
+    return "GSTify Backend is Running 🚀"
+
 if __name__ == '__main__':
     # Run the Flask app on port 5000
     app.run(debug=True, host='0.0.0.0', port=5000)
